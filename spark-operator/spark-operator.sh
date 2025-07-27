fb233fb29b43f455716f85c9c8079828cf3491ae
bhighnam <bhighnam@gmail.com>
2025-07-27 16:37:13 -0400

    got spark job to run

--- spark-operator/spark-operator.sh
+++ spark-operator/spark-operator.sh
@@ -6,3 +6,5 @@ helm install my-release spark-operator/spark-operator \
     --namespace spark-operator \
     --create-namespace \
     --set webhook.enable=true
+
+# do a kubectl get sa( service account ) to get the actual service account name. In this case it would be my-release-spark-operator-spark
